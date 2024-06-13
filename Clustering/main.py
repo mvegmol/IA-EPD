6ab99b5df3effc641cfc2d539a4ec2e19cf0c2d4
Miguel Vega Molina <mvegmol24@gmail.com>
2024-06-13 16:23:44 +0200

    Clustering, examen luis

--- Clustering/main.py
+++ Clustering/main.py
@@ -3,6 +3,78 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
 
+def findClosestCentroids(x, initial_centroids):
+    m = len(x)
+    k = len(initial_centroids)
+    idx = np.zeros(m, dtype=int)
+    for i in range(m):
+        min_dist = float('inf')
+        for j in range(k):
+            dist = np.linalg.norm(x[i] - initial_centroids[j])
+            if dist < min_dist:
+                min_dist = dist
+                idx[i]=j
+    return idx
+
+def computeCentroids(x, idx, k):
+    m,n = x.shape
+    centroids = np.zeros((k,n))
+    for j in range(k):
+        ejemplos_asignados = x[idx == j]
+        if len(ejemplos_asignados) > 0:
+            centroids[j] = np.mean(ejemplos_asignados, axis=0)
+
+    return centroids
+
+def runKmeans(x, initial_centroids, max_iter, plot=True):
+    centroides = initial_centroids
+    k = initial_centroids.shape[0]
+
+    for iter in range(max_iter):
+        #Los indices mas cercanos
+        idx = findClosestCentroids(x,centroides)
+        centroides = computeCentroids(x,idx,k)
+        if plot:
+            plt.scatter(x[:,0], x[:,1], c=idx, cmap='rainbow')
+            plt.scatter(centroides[:,0], centroides[:,1], marker='x', s=200,linewidths=3, color='black')
+            plt.title(f'Iteration {iter +1}')
+            plt.show()
+
+    return centroides, idx
+
+def kMeansInitCentroids(x, k):
+    # Número de ejemplos de entrenamiento
+    m = x.shape[0]
+    # Indices aleatorios
+    idx = np.random.randint(0, m, size=k)
+    # Seleccionar ejemplos aleatorios como centroides
+    centroids = x[idx]
+    return centroids
+
+
+def elbowMethod(X):
+    costes = []
+    for k in range(1, 10 + 1):
+        centroides, idx = runKmeans(X, kMeansInitCentroids(X, k), max_iter=10, plot=False)
+        cost = np.sum((X - centroides[idx]) ** 2) / (2 * len(X))
+        costes.append(cost)
+
+    # Calcular las diferencias entre los costos
+    diff = np.diff(costes)
+    # Encontrar el punto de codo utilizando la segunda derivada (diferencia de diferencias)
+    acceleration = np.diff(diff)
+
+    # Encontrar el punto de cambio más significativo en la aceleración
+    elbow_point = np.argmax(acceleration) + 2  # Sumamos 2 para compensar la segunda derivada
+
+    plt.plot(range(1, 10 + 1), costes, marker='o')
+    plt.title('Elbow Method')
+    plt.xlabel('Number of Clusters (K)')
+    plt.ylabel('Cost')
+    plt.show()
+    return elbow_point
+
+
 if __name__ == '__main__':
     print("Loading data and setting initial set of centroids\n")
     # Load an example dataset that we will be using
@@ -24,8 +96,8 @@ if __name__ == '__main__':
 
     # Find the closest centroids for the examples using the initial_centroids
     print("Finding closest centroids\n")
-    #idx = findClosestCentroids(X, initial_centroids)
-    #print("Closest centroids for the first 3 examples: ", idx[0:3], " (the closest centroids should be 0, 2, 1 respectively)\n")
+    idx = findClosestCentroids(X, initial_centroids)
+    print("Closest centroids for the first 3 examples: ", idx[0:3], " (the closest centroids should be 0, 2, 1 respectively)\n")
 
     ## ===================== EJ2: Compute Means =========================
     # After implementing the closest centroids function, you should now
@@ -34,9 +106,9 @@ if __name__ == '__main__':
     print("Computing centroids means\n")
 
     # Compute means based on the closest centroids found in the previous part
-    #centroids = computeCentroids(X, idx, K)
-    #print("Centroids computed after initial finding of closest centroids:\n", centroids,
-    #      "\n(the centroids should be [ [2.428301 3.157924] [5.813503 2.633656] [7.119387 3.616684] ] \n")
+    centroids = computeCentroids(X, idx, K)
+    print("Centroids computed after initial finding of closest centroids:\n", centroids,
+          "\n(the centroids should be [ [2.428301 3.157924] [5.813503 2.633656] [7.119387 3.616684] ] \n")
 
     ## =================== EJ3: K-Means Clustering ======================
     # After you have completed the two functions computeCentroids and
@@ -48,18 +120,18 @@ if __name__ == '__main__':
     print("Running K-Means clustering one example dataset")
     max_iters = 10
 
-    #centroids, idx = runKmeans(X, initial_centroids, max_iters, plot=True)
-    #print("Centroids computed after ", max_iters, " iterations of K-Means with manual initial centroids:\n", centroids)
+    centroids, idx = runKmeans(X, initial_centroids, max_iters, plot=False)
+    print("Centroids computed after ", max_iters, " iterations of K-Means with manual initial centroids:\n", centroids)
     print("\nK-Means done\n")
 
     ## =================== EJ4: Setting random initial centroids ======================
     # Setting centroids to random examples of the training data and ru-run the kmeans algorithm
-    #random_initial_centroids = kMeansInitCentroids(X, K)
-    #centroids, idx = runKmeans(X, random_initial_centroids, max_iters, plot=True)
-    #print("Centroids computed after ", max_iters, " iterations of K-Means with random initial centroids:\n", centroids)
+    random_initial_centroids = kMeansInitCentroids(X, K)
+    centroids, idx = runKmeans(X, random_initial_centroids, max_iters, plot=True)
+    print("Centroids computed after ", max_iters, " iterations of K-Means with random initial centroids:\n", centroids)
 
-    ## =================== EJ5: Elbow Method ======================
-    #elbowMethod(X)
+    ## =================== EJ5: Elbow Method ====================== ##
+    elbowMethod(X)
 
     ## =================== P1: K-Means with SKLEARN ======================
     print("\n\nPROBLEMS\n")
--- RedesNeuronales/main.py
+++ RedesNeuronales/main.py
@@ -1,14 +1,97 @@
 import numpy as np
+import pandas as pd
 import scipy.io as sio
 import scipy.optimize as opt
 
 
-from checkNNGradients import checkNNGradients
+#from checkNNGradients import checkNNGradients
 #from nnCostFunctionSinReg import nnCostFunctionSinReg
 #from randInitializeWeights import randInitializeWeights
 #from nnGradFunctionSinReg import nnGradFunctionSinReg
 #from predict import predict
+def nnGradFunctionSinReg(nn_params_ini, input_layer_size, hidden_layer_size, num_labels, X, y):
+    # Paso 1: Saco theta1 y theta2 de nn_params_ini
+    theta1 = np.reshape(a=nn_params_ini[:hidden_layer_size * (input_layer_size + 1)],
+                        newshape=(hidden_layer_size, input_layer_size + 1),
+                        order="F")
 
+    theta2 = np.reshape(a=nn_params_ini[hidden_layer_size * (input_layer_size + 1):],
+                        newshape=(num_labels, hidden_layer_size + 1),
+                        order="F")
+
+    # Paso 2: One hot encoding
+    y_d = pd.get_dummies(y.flatten())  # y es un arrayNumpy // si y es Dataframe habría que hacer y.to_numpy().flatten()
+    # Paso 3: Para cada fila de X
+    delta1 = np.zeros(theta1.shape)
+    delta2 = np.zeros(theta2.shape)
+    for i in range(len(X)):
+        # Paso 3.1: Llamo al forward para sacar a1, a2 y a3(h)
+        a1, a2, a3 = forward(theta1, theta2, X, i)
+        # Paso 3.2: Calculo los errores (en capa 1 no hay)
+        d3 = a3 - y_d.iloc[i]
+        d2_parte1 = np.dot(theta2.T, d3)
+        d2_parte2 = np.multiply(a2, 1 - a2)
+        d2 = np.multiply(d2_parte1, d2_parte2)
+        # Paso 3.3: Acumulo el error en delta1 y delta2
+        delta1_parte1 = np.reshape(a = d2[1:], newshape = (hidden_layer_size, 1)) # Quito la bias
+        delta1_parte2 = np.reshape(a = a1, newshape = (1, input_layer_size + 1))
+        delta1 = delta1 + np.dot(delta1_parte1, delta1_parte2)
+        delta2_parte1 = np.reshape(a = d3.to_numpy() #d3 es una Serie. Para aplicar el np.reshape (de numpy) tengo que convertirlo a numpy/array
+                                        ,newshape=(num_labels, 1))
+        delta2_parte2 = np.reshape(a = a2, newshape = (1, hidden_layer_size + 1))
+        delta2 = delta2 + np.dot(delta2_parte1, delta2_parte2)
+    m = len(X)
+    delta1 = delta1 / m
+    delta2 = delta2 / m
+    return np.hstack((delta1.ravel(order = "F"), delta2.ravel(order = "F")))
+
+def nnCostFunctionSinReg(nn_params_ini, input_layer_size, hidden_layer_size, num_labels, X, y):
+    #Sacamos las thetas 1 t theta 2 del nn_params_ini
+    theta1 = np.reshape(a=nn_params_ini[:hidden_layer_size *(input_layer_size+1)],
+                        newshape=(hidden_layer_size, input_layer_size+1),
+                        order="F")
+    theta2 = np.reshape(a= nn_params_ini[hidden_layer_size *(input_layer_size+1):],
+                        newshape=(num_labels, hidden_layer_size+1),
+                        order="F")
+    #Paso 2 : One hot coding
+    y_d = pd.get_dummies(y.flatten())  # y es un arrayNumpy // si y es Dataframe habría que hacer y.to_numpy().flatten()
+    y_d = y_d.astype("int")
+    suma=0
+    #Para cada fila X
+    for i in range(len(X)):
+        # Paso 3.1: Forward // Feetforward
+        a1,a2, h = forward(theta1,theta2,X,i)
+        # Paso 3.2: Coste como regresión logística
+        y_1 = y_d.iloc[i] * np.log(h)
+        y_0 = (1 - y_d.iloc[i]) * (np.log(1 - h))
+        temp = np.sum(y_1 + y_0)
+        suma = suma + temp
+
+    m = len(X)
+    J = -(1 / m) * suma
+    return J
+def forward(theta1, theta2, X, i): # i es la fila
+    a1 = np.hstack((1, X[i])) # Le pongo la bias
+    a2 = sigmoide(np.dot(theta1, a1))
+    a2 = np.hstack((1, a2)) # Le pongo la bias
+    h = sigmoide(np.dot(theta2, a2)) # No se pone la bias con la última capa
+    return a1, a2, h
+
+
+def sigmoide(z):
+    return 1 / (1 + np.exp(-z))
+
+def randInitializeWeights(layer_input, layer_output): #Forward (me quedo con h = a3)
+    epsilon = 0.12
+    return np.random.rand(layer_output, layer_input + 1) * (2 * epsilon) - epsilon
+
+def predict(theta1, theta2, X):
+    ones = np.ones((len(X), 1))
+    a1 = np.hstack((ones, X))
+    a2 = sigmoide(np.dot(a1, theta1.T))
+    a2 = np.hstack((ones, a2))
+    h = sigmoide(np.dot(a2, theta2.T))
+    return np.argmax(h, axis = 1) + 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -36,33 +119,14 @@ if __name__ == '__main__':
 
 
     ## ================ EJ1. Compute Cost (Feedforward) ================
-    # To the neural network, you should first start by implementing the
-    # forward part of the neural network that returns the cost only.  After
-    # implementing the forward propagation to compute the cost, you can verify that
-    # your implementation is correct by verifying that you get the same cost
-    # as us for the fixed debugging parameters.
-    #
-    # We suggest implementing the feedforward cost *without* regularization
-    # first so that it will be easier for you to debug.
-    #
     print("Feedforward Using Neural Network... \n")
-    #J = nnCostFunctionSinReg(nn_params_ini, input_layer_size, hidden_layer_size, num_labels, X, y)
-    #print("Cost at parameters (loaded from ex4weights) (this value should be about 0.287629): ", J)
+    J = nnCostFunctionSinReg(nn_params_ini, input_layer_size, hidden_layer_size, num_labels, X, y)
+    print("Cost at parameters (loaded from ex4weights) (this value should be about 0.287629): ", J)
 
     ## =============== EJ2. Implement Backpropagation ===============
-    # You should proceed to implement the backpropagation algorithm
-    # for the neural network. You should implement a new function
-    # nnGradFunctionSinReg to return the partial
-    # derivatives of the parameters.
-    #
-    # In order to be sure the derivatives are calculated correctly,
-    # Use the checkNNGradients(lambda_param) function that creates
-    # a small neural network to check the backpropagation gradients
-    #
-    # Check EB T5 Parte II slides: 19, 20
     print("\nChecking Backpropagation... \n")
     lambda_param = 0
-    #checkNNGradients(lambda_param) # Check gradients by running checkNNGradients
+
 
     # ================ EJ3. Initializing Parameters ================
     # In this part of the exercise, you will start by
@@ -71,32 +135,25 @@ if __name__ == '__main__':
     #
     # Check EB T5 Parte II slides: 21, 22
     print("\nInitializating Neural Network Parameters ...\n")
-    # initial_theta1 = randInitializeWeights(input_layer_size, hidden_layer_size)
-    # initial_theta2 = randInitializeWeights(hidden_layer_size, num_labels)
+    initial_theta1 = randInitializeWeights(input_layer_size, hidden_layer_size)
+    initial_theta2 = randInitializeWeights(hidden_layer_size, num_labels)
     # Unroll parameters (a single column vector)
-    # nn_initial_params = np.hstack((initial_theta1.ravel(order='F'), initial_theta2.ravel(order='F')))
+    nn_initial_params = np.hstack((initial_theta1.ravel(order='F'), initial_theta2.ravel(order='F')))
 
     ## =================== EJ4. Training NN ===================
-    # You have now implemented all the code necessary to train a neural
-    # network. To train your neural network, we will now use "fmin_cg".
-    # Recall that these advanced optimizers are able to train our cost
-    # functions efficiently as long as we provide them with the
-    # gradient computations.
-    #
-
     print("Training Neural Network ...\n")
     # After you have completed the assignment, change the MaxIter to a larger
     # value to see how more training helps.
     maxiter = 50
     # The cost function needs to be minimized
-    #nn_params = opt.fmin_cg(maxiter=maxiter, f=nnCostFunctionSinReg, x0=nn_initial_params, fprime=nnGradFunctionSinReg,
-    #                       args=(input_layer_size, hidden_layer_size, num_labels, X, y.flatten()))
+    nn_params = opt.fmin_cg(maxiter=maxiter, f=nnCostFunctionSinReg, x0=nn_initial_params, fprime=nnGradFunctionSinReg,
+                           args=(input_layer_size, hidden_layer_size, num_labels, X, y.flatten()))
 
     # Obtain theta1 and theta2 back from nn_params
-    #theta1 = np.reshape(nn_params[:hidden_layer_size * (input_layer_size + 1)],
-    #                        (hidden_layer_size, input_layer_size + 1), 'F')
-    #theta2 = np.reshape(nn_params[hidden_layer_size * (input_layer_size + 1):], (num_labels, hidden_layer_size + 1),
-    #                        'F')
+    theta1 = np.reshape(nn_params[:hidden_layer_size * (input_layer_size + 1)],
+                            (hidden_layer_size, input_layer_size + 1), 'F')
+    theta2 = np.reshape(nn_params[hidden_layer_size * (input_layer_size + 1):], (num_labels, hidden_layer_size + 1),
+                           'F')
 
     ## ================= EJ5. Implement Predict =================
     # After training the neural network, we would like to use it to predict
